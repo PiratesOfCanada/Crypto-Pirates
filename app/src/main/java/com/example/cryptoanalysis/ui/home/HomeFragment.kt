6c1d8beb3e1ad87dc9d40ff32cfa72e503cbd74e
jinal.parmar@greylabs.com <jinalparmar45@gmail.com>
2022-06-24 13:17:41 -0400

    Homepage favourites loader

--- app/src/main/java/com/example/cryptoanalysis/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cryptoanalysis/ui/home/HomeFragment.kt
@@ -1,13 +1,20 @@
 package com.example.cryptoanalysis.ui.home
 
+import android.R
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.SimpleAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptoanalysis.Coin
 import com.example.cryptoanalysis.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
 
@@ -28,10 +35,43 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val recyclerview :RecyclerView = binding.recyclerView
+
+        var list = ArrayList<Coin>()
+        list.add(Coin(name = "Bitcoin", symbol = "BTC", color = "#f7931A", price = 21173.51734301634f, iconUrl = "https://cdn.coinranking.com/bOabBYkcX/bitcoin_btc.svg"))
+
+        recyclerview.layoutManager = LinearLayoutManager(requireContext())
+        val RecyclerAdapter = RecyclerAdapter(list, requireContext())
+
+        RecyclerAdapter.setItemListener(object : RecyclerAdapter.onItemClickListener{
+            override fun onClickListener(position: Int) {
+                Log.d("Clicked", "element - ${list[position]}")
+            }
+
+        })
+        recyclerview.adapter = RecyclerAdapter
+//        val textView: TextView = binding.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+
+
+//        for(i in 0..10){
+//            val map: HashMap<String, String> = HashMap()
+//            map["line1"] = "Foo $i"
+//            map["line2"] = "Bar $i"
+//            list.add(map)
+//        }
+//        val from = arrayOf("line1", "line2")
+//        val to = intArrayOf(R.id.text1, R.id.text2)
+//        val adapter  = SimpleAdapter(
+//            requireContext(), list,
+//            R.layout.simple_list_item_2, from, to
+//        )
+//        //setListAdapter(adapter)
+//
+//        RecyclerView.Adapter = adapter
+
         return root
     }
 
---
+++ app/src/main/java/com/example/cryptoanalysis/ui/home/RecyclerAdapter.kt
@@ -0,0 +1,128 @@
+package com.example.cryptoanalysis.ui.home
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import coil.ImageLoader
+import coil.decode.SvgDecoder
+import coil.load
+import coil.request.ImageRequest
+import com.example.cryptoanalysis.Coin
+import com.example.cryptoanalysis.R
+import com.squareup.picasso.Picasso
+
+class RecyclerAdapter(val DataSourse : ArrayList<Coin>, val context : Context):  RecyclerView.Adapter<ViewHolder>() {
+
+
+    private lateinit var  ItemListener : onItemClickListener;
+    interface onItemClickListener{
+        fun onClickListener(position : Int)
+    }
+
+
+    fun setItemListener(Listener: onItemClickListener){
+        this.ItemListener = Listener;
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent,false)
+        return ViewHolder(view, ItemListener)
+    }
+
+//    @SuppressLint("Range")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val itemVH = DataSourse[position]
+        holder.currencyname.text = itemVH.name
+        holder.currencyname.setTextColor( Color.parseColor(itemVH.color))
+       holder.price.text = itemVH.price.toString()
+        holder.price.setTextColor( Color.parseColor(itemVH.color))
+
+    //to load svg file
+    holder.symbol.loadUrl(itemVH.iconUrl)
+
+    }
+
+    override fun getItemCount(): Int {
+        return DataSourse.size;
+    }
+
+
+    fun ImageView.loadUrl(url: String) {
+
+        val imageLoader = ImageLoader.Builder(this.context)
+            .componentRegistry { add(SvgDecoder(this@loadUrl.context)) }
+            .build()
+
+        val request = ImageRequest.Builder(this.context)
+            .crossfade(true)
+            .crossfade(500)
+           // .placeholder(R.drawable.placeholder)
+           // .error(R.drawable.error)
+            .data(url)
+            .target(this)
+            .build()
+
+        imageLoader.enqueue(request)
+    }
+
+
+}
+
+//private fun ImageView.loadSvg(iconUrl: String, symbol: ImageView) {
+//
+//}
+//fun AppCompatImageView.loadSvg(url: String) {
+//    val imageLoader = ImageLoader.Builder(this.context)
+//        .componentRegistry { add(SvgDecoder(this@loadSvg.context)) }
+//        .build()
+//
+//    val request = ImageRequest.Builder(this.context)
+//        .crossfade(true)
+//        .crossfade(500)
+//        .data(url)
+//        .target(onSuccess = {img->})
+//        .build()
+//
+//
+//    imageLoader.enqueue(request)
+//}
+class ViewHolder(view: View, ItemListener: RecyclerAdapter.onItemClickListener) : RecyclerView.ViewHolder(view) {
+    var currencyname: TextView = view.findViewById(R.id.currencyname)
+    var symbol: ImageView = view.findViewById(R.id.imageView)
+    var price: TextView = view.findViewById(R.id.price)
+
+    init{
+        itemView.setOnClickListener {
+            ItemListener.onClickListener(adapterPosition)
+        }
+
+
+    }
+
+}
+
+abstract class onRightSwipeItem :  ItemTouchHelper.Callback() {
+    override fun getMovementFlags(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder
+    ): Int {
+        return makeMovementFlags(0, ItemTouchHelper.RIGHT)
+    }
+
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
+        return false
+    }
+
+}
