b43739d35117cf5875dde8f84306ca199cd7b34b
jinal.parmar@greylabs.com <jinalparmar45@gmail.com>
2022-07-02 13:00:48 -0400

    News display

--- app/src/main/java/com/example/cryptoanalysis/ui/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/cryptoanalysis/ui/viewmodel/NewsViewModel.kt
@@ -1,13 +1,23 @@
 package com.example.cryptoanalysis.ui.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.cryptoanalysis.data.api.Api
+import com.example.cryptoanalysis.data.model.ResponseCoins
+import com.example.cryptoanalysis.data.model.ResponseNews
+import com.example.cryptoanalysis.data.repo.NewsRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.core.Observable
+import javax.inject.Inject
 
-class NewsViewModel : ViewModel() {
+@HiltViewModel
+class NewsViewModel @Inject constructor(var repo: NewsRepo) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+    fun getAllNews(): Observable<ResponseNews> {
+        Api.seturl("https://api.goperigon.com")
+        //Log.d("Retrofit: vm ", "${Api.geturl()}")
+        return repo.getAllNews()
     }
-    val text: LiveData<String> = _text
 }
